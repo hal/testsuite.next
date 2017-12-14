395685bf5102a005875a53e59e90a4c80bd7f26f
Harald Pehl <harald.pehl@gmail.com>
2017-12-14 17:28:01 +0100

    Add scripts to start and stop halconsole/hal-wildfly docker image

--- wildfly.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -p 8080:8080 -p 9990:9990 -it halconsole/hal-wildfly /opt/jboss/wildfly/bin/standalone.sh -c standalone-full-ha-insecure.xml -b 0.0.0.0 -bmanagement 0.0.0.0
