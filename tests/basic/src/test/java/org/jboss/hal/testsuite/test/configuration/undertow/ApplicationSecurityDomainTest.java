2d420305aceb3f4eb616daf4216346ff5cd303ce
spriadka <spriadka@github.com>
2018-03-26 14:08:41 +0200

    added GlobalSettingsTest and GlobalSettingsFinderTest

---
+++ tests/basic/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/ApplicationSecurityDomainTest.java
@@ -0,0 +1,8 @@
+package org.jboss.hal.testsuite.test.configuration.undertow;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ApplicationSecurityDomainTest {
+}
