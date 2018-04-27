c7f4db6b69872cfc845f7f9dee8cf2b88b9fee8c
spriadka <spriadka@github.com>
2018-04-27 11:22:16 +0200

    fixed network attaching

--- start-wildfly.sh
+++ start-wildfly.sh
@@ -6,4 +6,4 @@ NETWORK=testnetwork
 # This is due to ModClusterFilterTest and ModClusterFilterAttributesTest,
 # as they require the server not to be bound to the 0.0.0.0 address
 docker network create --subnet 172.80.0.0/28 testnetwork
-docker run -p 8080:8080 -p 9990:9990 -d --name ${CONTAINER} halconsole/hal-wildfly /opt/jboss/wildfly/bin/standalone.sh -c standalone-full-ha-insecure.xml -b 172.80.0.2 -bmanagement 172.80.0.2
+docker run -p 8080:8080 -p 9990:9990 -d --name ${CONTAINER} --network ${NETWORK} halconsole/hal-wildfly /opt/jboss/wildfly/bin/standalone.sh -c standalone-full-ha-insecure.xml -b 172.80.0.2 -bmanagement 172.80.0.2
