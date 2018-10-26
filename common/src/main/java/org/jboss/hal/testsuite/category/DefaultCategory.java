195fe37cb9f604bde961d3bab3532f734fbe9a59
Pavel Jelinek <zebriking@gmail.com>
2018-10-26 13:21:36 +0200

    Run only Domain category annotated tests in domain profile.
    Motivation: not annotated tests are not prepared to be run in domain mode so far.

---
+++ common/src/main/java/org/jboss/hal/testsuite/category/DefaultCategory.java
@@ -0,0 +1,6 @@
+package org.jboss.hal.testsuite.category;
+
+/** JUnit category for configuration use only. Please do not use this as category of any test class!*/
+public interface DefaultCategory {
+
+}
