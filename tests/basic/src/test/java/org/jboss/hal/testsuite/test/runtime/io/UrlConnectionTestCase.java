0f9c8a60552721cc224b01725236706b3c2e38c0
Petr Adamec <padamec@redhat.com>
2019-05-28 20:28:34 +0200

    [JBEAP-16671] - Console shows server URL as http://[host]:webPort even if http is disabled -Add test

--- tests/basic/src/test/java/org/jboss/hal/testsuite/test/runtime/io/UrlConnectionTestCase.java
+++ tests/basic/src/test/java/org/jboss/hal/testsuite/test/runtime/io/UrlConnectionTestCase.java
@@ -14,9 +14,7 @@ import org.jboss.hal.testsuite.creaper.ManagementClientProvider;
 import org.jboss.hal.testsuite.creaper.command.BackupAndRestoreAttributes;
 import org.jboss.hal.testsuite.fragment.finder.FinderFragment;
 import org.jboss.hal.testsuite.fragment.finder.FinderPath;
-import org.jboss.hal.testsuite.fragment.finder.ItemFragment;
 import org.jboss.hal.testsuite.fragment.finder.ServerPreviewFragment;
-import org.jboss.hal.testsuite.test.configuration.undertow.UndertowFixtures;
 import org.jboss.hal.testsuite.util.ConfigUtils;
 import org.jboss.hal.testsuite.util.ServerEnvironmentUtils;
 import org.junit.AfterClass;
@@ -26,38 +24,65 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.wildfly.extras.creaper.core.CommandFailedException;
 import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
-import org.wildfly.extras.creaper.core.online.operations.OperationException;
+import org.wildfly.extras.creaper.core.online.operations.Address;
+import org.wildfly.extras.creaper.core.online.operations.Batch;
 import org.wildfly.extras.creaper.core.online.operations.Operations;
+import org.wildfly.extras.creaper.core.online.operations.Values;
 import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
-import static org.jboss.hal.testsuite.test.configuration.elytron.ElytronFixtures.SUBSYSTEM_ADDRESS;
 
 
 @RunWith(Arquillian.class)
 @Category(Domain.class)
 public class UrlConnectionTestCase {
+    private static final String SERVER_GROUP = "server-group";
+    private static final String PROFILE = "profile";
+    private static final String DEFAULT = "default";
 
     private static final OnlineManagementClient client = ManagementClientProvider.createOnlineManagementClient();
     private static final ServerEnvironmentUtils serverEnvironmentUtils = new ServerEnvironmentUtils(client);
     private static final Operations operations = new Operations(client);
-    private static final BackupAndRestoreAttributes backup = new BackupAndRestoreAttributes.Builder(SUBSYSTEM_ADDRESS).build();
+
+    private static final Address hostMaster = Address.of("host","master");
+    private static final Address serverGroupMain = Address.of(SERVER_GROUP,"main-server-group");
+
+    private static final BackupAndRestoreAttributes backupHost = new BackupAndRestoreAttributes.Builder(hostMaster).build();
+    private static final BackupAndRestoreAttributes backupOtherServerGroup = new BackupAndRestoreAttributes.Builder(Address.of(SERVER_GROUP,"other-server-group")).build();
+    private static final BackupAndRestoreAttributes backupMainServerGroup = new BackupAndRestoreAttributes.Builder(Address.of(SERVER_GROUP,"main-server-group")).build();
+    private static final BackupAndRestoreAttributes backupProfile = new BackupAndRestoreAttributes.Builder(Address.of(PROFILE, DEFAULT)).build();
     private static final Administration administration = new Administration(client);
 
     @BeforeClass
-    public static void setUp() throws IOException, OperationException, CommandFailedException {
-        client.apply(backup.backup());
-        if (operations.exists(UndertowFixtures.httpListenerAddress("default-server", "default"))) {
-            operations.removeIfExists(UndertowFixtures.httpListenerAddress("default-server", "default"));
-        }
+    public static void setUp() throws IOException, TimeoutException, InterruptedException {
+        backupHost.backup();
+        backupOtherServerGroup.backup();
+        backupMainServerGroup.backup();
+        backupProfile.backup();
+
+        operations.batch(new Batch().remove(hostMaster.and("server-config","server-three"))); // /host=master/server-config=server-three:remove()
+        operations.batch(new Batch().remove(Address.of(SERVER_GROUP,"other-server-group")));                  // /server-group=other-server-group:remove()
+
+        operations.batch(new Batch().add(serverGroupMain, Values.of(PROFILE, DEFAULT)));
+        operations.batch(new Batch().writeAttribute(serverGroupMain,PROFILE, DEFAULT));
+
+        operations.batch(new Batch().writeAttribute(serverGroupMain,"socket-binding-group","standard-sockets"));
+
+        operations.batch(new Batch().remove(Address.of(PROFILE,DEFAULT).and("subsystem","undertow").and("server","default-server").and("http-listener","default")));
+        operations.batch(new Batch().writeAttribute(serverGroupMain.and("jvm",DEFAULT),"heap-size", "256m"));
+        operations.batch(new Batch().writeAttribute(serverGroupMain.and("jvm",DEFAULT),"max-heap-size", "256m"));
+
+        administration.reloadIfRequired();
 
     }
     @AfterClass
     public static void tearDown()
-            throws IOException, CommandFailedException, OperationException, TimeoutException, InterruptedException, TimeoutException {
+            throws IOException, InterruptedException, TimeoutException {
         try {
-            client.apply(backup.restore());
+            backupHost.restore();
+            backupOtherServerGroup.restore();
+            backupMainServerGroup.restore();
+            backupProfile.restore();
             administration.reloadIfRequired();
         } finally {
             client.close();
@@ -72,48 +97,14 @@ public class UrlConnectionTestCase {
     private Console console;
 
     @Test
-    public void editUrl() throws IOException {
-        FinderFragment fragment = getMainAttributesFinder();
-        selectServer(fragment);
+    public void testUrlHttps() throws IOException {
+        FinderFragment fragment = console.finder(NameTokens.RUNTIME, new FinderPath()
+                .append(Ids.DOMAIN_BROWSE_BY, "hosts")
+                .append(Ids.HOST, Ids.build("host", ConfigUtils.getDefaultHost())));
+        fragment.column(Ids.SERVER).selectItem(Ids.build(ConfigUtils.getDefaultHost(), ConfigUtils.getDefaultServer()));
         ServerPreviewFragment serverPreviewFragment = fragment.preview(ServerPreviewFragment.class);
         if (!serverPreviewFragment.getUrlAttributeItem().getValueElement().getText().contains("https")) {
-            Assert.fail("URL should contain https!");
-        }
-    }
-
-
-
-
-    public void verifyBrowserRedirected(String url, String assertMessage) {
-        String redirectUrlExpression = "^" + url + "[/]?$";
-        for (String windowHandle : browser.getWindowHandles()) {
-            browser.switchTo().window(windowHandle);
-            if (browser.getCurrentUrl().matches(redirectUrlExpression)) {
-                return;
-            }
-        }
-        Assert.fail(assertMessage);
-    }
-
-    private FinderFragment getMainAttributesFinder() {
-        FinderFragment fragment;
-        if (ConfigUtils.isDomain()) {
-            fragment = console.finder(NameTokens.RUNTIME, new FinderPath()
-                    .append(Ids.DOMAIN_BROWSE_BY, "hosts")
-                    .append(Ids.HOST, Ids.build("host", ConfigUtils.getDefaultHost())));
-        } else {
-            fragment = console.finder(NameTokens.RUNTIME);
-        }
-        return fragment;
-    }
-
-    private ItemFragment selectServer(FinderFragment fragment) throws IOException {
-        if (ConfigUtils.isDomain()) {
-            return fragment.column(Ids.SERVER)
-                    .selectItem(Ids.build(ConfigUtils.getDefaultHost(), ConfigUtils.getDefaultServer()));
-        } else {
-            return fragment.column(Ids.STANDALONE_SERVER_COLUMN)
-                    .selectItem("standalone-host-" + serverEnvironmentUtils.getServerHostName());
+            Assert.fail("URL should contain https! See WFLY-12115");
         }
     }
 }
