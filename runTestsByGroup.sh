6ae64eeb8234d3d6fe572afafea0807b781b4da6
Harald Pehl <harald.pehl@gmail.com>
2020-10-22 09:16:52 +0200

    Add '-r' option to read (as suggested by shell extensions)

--- runTestsByGroup.sh
+++ runTestsByGroup.sh
@@ -3,7 +3,7 @@
 # mvn test -P chrome,basic,standalone -Dtest=org.jboss.hal.testsuite.test.configuration.messaging.*Test
 
 find tests/basic/src/test/java/org/jboss/hal/testsuite/test -type f -name \*Test.java | sed 's/\/[a-Z]*\.java$//g' | sort | uniq \
-    | sort | sed 's/tests\/basic\/src\/test\/java\///g;s/\//\./g' | while read pkg;
-do 
+    | sort | sed 's/tests\/basic\/src\/test\/java\///g;s/\//\./g' | while read -r pkg;
+do
     mvn test -P chrome,basic,standalone -Dtest=$pkg.*Test
 done
