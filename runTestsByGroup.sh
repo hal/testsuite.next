b4df8749c7a8e0db746c6c0e09a39d4434ac0246
Harald Pehl <harald.pehl@gmail.com>
2020-10-23 12:45:36 +0200

    Fix regexp and reformat

--- runTestsByGroup.sh
+++ runTestsByGroup.sh
@@ -2,8 +2,13 @@
 # and output it as an mvn command to run each packages as
 # mvn test -P chrome,basic,standalone -Dtest=org.jboss.hal.testsuite.test.configuration.messaging.*Test
 
-find tests/basic/src/test/java/org/jboss/hal/testsuite/test -type f -name \*Test.java | sed 's/\/[a-Z]*\.java$//g' | sort | uniq \
-    | sort | sed 's/tests\/basic\/src\/test\/java\///g;s/\//\./g' | while read -r pkg;
+find tests/basic/src/test/java/org/jboss/hal/testsuite/test -type f -name \*Test.java \
+ | sed 's/\/[a-zA-Z0-9]*\.java$//g' \
+ | sort \
+ | uniq \
+ | sort \
+ | sed 's/tests\/basic\/src\/test\/java\///g;s/\//\./g' \
+ | while read -r pkg;
 do
-    mvn test -P chrome,basic,standalone -Dtest=$pkg.*Test
+  mvn test -P chrome,basic,standalone -Dtest=$pkg.*Test
 done
