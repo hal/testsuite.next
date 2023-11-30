ca5821321aab983c40517399d7ba4a55c9089d0e
mslovik <mslovik@redhat.com>
2023-11-30 10:45:38 +0100

    Add server configuration check

--- prepare.sh
+++ prepare.sh
@@ -18,25 +18,36 @@ echo "Server directory: $SERVER_DIR"
 # Set JBOSS_HOME
 export JBOSS_HOME=$SERVER_DIR
 
+# Check if the server has "master" or "primary" configuration
+if find $SERVER_DIR -name "host-primary.xml" -print -quit | grep -q .; then
+    HOST="primary"
+    HOST_CONFIG_FILE="host-primary.xml"
+else
+    HOST="master"
+    HOST_CONFIG_FILE="host-master.xml"
+fi
+echo "Host: $HOST"
+echo "Host configuration file: $HOST_CONFIG_FILE"
+
 if [ "$MODE" == "domain" ]; then
-    $SERVER_DIR/bin/domain.sh &
-    $SERVER_DIR/bin/jboss-cli.sh -c '/host=primary/core-service=management/management-interface=http-interface
+    $SERVER_DIR/bin/domain.sh --host-config=$HOST_CONFIG_FILE &
+    $SERVER_DIR/bin/jboss-cli.sh -c "/host=$HOST/core-service=management/management-interface=http-interface
                                       :undefine-attribute(name=security-realm)
-                                      :reload'
+                                      :reload"
     sleep 40
 elif [ "$MODE" == "domain-hc-dc" ]; then
     # Setup dc, please setup hc manually
     cp -r $SERVER_DIR/domain/ $SERVER_DIR/dc/
-    $SERVER_DIR/bin/domain.sh --host-config=host-master.xml -Djboss.domain.base.dir=$SERVER_DIR/dc &
-    $SERVER_DIR/bin/jboss-cli.sh -c '/host=master/core-service=management/management-interface=http-interface
+    $SERVER_DIR/bin/domain.sh --host-config=$HOST_CONFIG_FILE -Djboss.domain.base.dir=$SERVER_DIR/dc &
+    $SERVER_DIR/bin/jboss-cli.sh -c "/host=$HOST/core-service=management/management-interface=http-interface
                                       :undefine-attribute(name=security-realm)
-                                     /host=master:reload'
+                                     /host=$HOST:reload"
     sleep 40
 elif [ "$MODE" == "standalone" ]; then
     $SERVER_DIR/bin/standalone.sh -c standalone-full-ha.xml &
-    $SERVER_DIR/bin/jboss-cli.sh -c '/core-service=management/management-interface=http-interface
+    $SERVER_DIR/bin/jboss-cli.sh -c "/core-service=management/management-interface=http-interface
                                       :undefine-attribute(name=security-realm)
-                                      :reload'
+                                      :reload"
     sleep 40
 else
     echo "Error: Unknown mode. Specify either domain, domain-hc-dc or standalone"
--- shutdown.sh
+++ shutdown.sh
@@ -6,14 +6,21 @@ MODE=$2
 # Locate server directory
 SERVER_DIR="$(jar tf ${SERVER_ZIP} | head -1 | cut -d '/' -f 1)"
 
+# Check if the server has "master" or "primary" configuration
+if find $SERVER_DIR -name "host-primary.xml" -print -quit | grep -q .; then
+    HOST="primary"
+else
+    HOST="master"
+fi
+
 if [ "$MODE" == "domain" ]; then
-    $SERVER_DIR/bin/jboss-cli.sh -c '/host=master:shutdown'
+    $SERVER_DIR/bin/jboss-cli.sh -c "/host=$HOST:shutdown"
 elif [ "$MODE" == "domain-hc-dc" ]; then
-    $SERVER_DIR/bin/jboss-cli.sh -c '/host=master:shutdown'
+    $SERVER_DIR/bin/jboss-cli.sh -c "/host=$HOST:shutdown"
     # Clean dc, please clean hc manually
     rm -r -d $SERVER_DIR/dc/*
 elif [ "$MODE" == "standalone" ]; then
-    $SERVER_DIR/bin/jboss-cli.sh -c ':shutdown'
+    $SERVER_DIR/bin/jboss-cli.sh -c ":shutdown"
 else
     echo "Error: Unknown mode. Specify either domain, domain-hc-dc or standalone"
 fi
