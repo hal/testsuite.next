d60635f321b5f2957093f260bbebaf0ab0b94856
khermano <khermano@redhat.com>
2026-01-27 15:34:04 +0100

    [EAPQE-4810] - Fixing failing test in webconsole testsuite caused by missing attribute

--- tests-configuration-undertow/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/server/listener/HTTPListenerConfigurationTest.java
+++ tests-configuration-undertow/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/server/listener/HTTPListenerConfigurationTest.java
@@ -23,6 +23,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.wildfly.extras.creaper.core.CommandFailedException;
 import org.wildfly.extras.creaper.core.online.ModelNodeResult;
@@ -31,6 +32,9 @@ import org.wildfly.extras.creaper.core.online.operations.Address;
 import org.wildfly.extras.creaper.core.online.operations.OperationException;
 import org.wildfly.extras.creaper.core.online.operations.Operations;
 import org.wildfly.extras.creaper.core.online.operations.Values;
+
+import static org.junit.Assume.assumeNoException;
+import static org.junit.Assume.assumeTrue;
 
 @RunWith(Arquillian.class)
 public class HTTPListenerConfigurationTest {
@@ -355,12 +359,16 @@ public class HTTPListenerConfigurationTest {
 
     @Test
     public void toggleRequireHostHttp11() throws Exception {
-        boolean requireHostHttp11 = operations.readAttribute(
-            HTTP_LISTENER_ADDRESS,
-            "require-host-http11").booleanValue();
-        crudOperations.update(
-            HTTP_LISTENER_ADDRESS,
-            page.getHttpListenerForm(), "require-host-http11", !requireHostHttp11);
+        ModelNodeResult result = operations.readAttribute(HTTP_LISTENER_ADDRESS, "require-host-http11");
+        assumeTrue(result.isDefined()); // if the attribute is not available in this configuration, skip the test
+        boolean requireHostHttp11 = result.booleanValue();
+        try {
+            crudOperations.update(
+                    HTTP_LISTENER_ADDRESS,
+                    page.getHttpListenerForm(), "require-host-http11", !requireHostHttp11);
+        } catch (NoSuchElementException e) {
+            assumeNoException(e); // the UI element is not available in this configuration, skip the test
+        }
     }
 
     @Test
