0509c9385176af4700828bda89ca14381d94b225
Ondrej Kotek <okotek@redhat.com>
2026-08-21 13:23:42 +0200

    Updating AJP tests to work with CVE-2026-15554 fix

--- tests-configuration-undertow/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/server/listener/AJPListenerTest.java
+++ tests-configuration-undertow/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/server/listener/AJPListenerTest.java
@@ -1,12 +1,14 @@
 package org.jboss.hal.testsuite.test.configuration.undertow.server.listener;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.dmr.ModelNode;
 import org.jboss.hal.resources.Ids;
 import org.jboss.hal.testsuite.Console;
 import org.jboss.hal.testsuite.CrudOperations;
@@ -23,9 +25,11 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.wildfly.extras.creaper.core.CommandFailedException;
 import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
+import org.wildfly.extras.creaper.core.online.operations.Address;
 import org.wildfly.extras.creaper.core.online.operations.OperationException;
 import org.wildfly.extras.creaper.core.online.operations.Operations;
 import org.wildfly.extras.creaper.core.online.operations.Values;
+import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
 @RunWith(Arquillian.class)
 public class AJPListenerTest {
@@ -62,7 +66,7 @@ public class AJPListenerTest {
         "socket-binding-to-be-removed-" + RandomStringUtils.randomAlphanumeric(7);
 
     @BeforeClass
-    public static void setUp() throws IOException, CommandFailedException {
+    public static void setUp() throws IOException, CommandFailedException, InterruptedException, TimeoutException {
         operations.add(UndertowFixtures.serverAddress(UNDERTOW_SERVER_TO_BE_TESTED));
         client.apply(new AddLocalSocketBinding(SOCKET_BINDING_TO_BE_ADDED));
         client.apply(new AddLocalSocketBinding(SOCKET_BINDING_TO_BE_REMOVED));
@@ -70,6 +74,10 @@ public class AJPListenerTest {
             UndertowFixtures.ajpListenerAddress(UNDERTOW_SERVER_TO_BE_TESTED, AJP_LISTENER_TO_BE_REMOVED),
             Values
                 .of("socket-binding", SOCKET_BINDING_TO_BE_REMOVED.toLowerCase() + "ref"));
+        // Adjusting REQUIRE_AJP_SECRET to satisfy the CVE-2026-15554 fix, see https://access.redhat.com/solutions/7146626
+        operations.add(Address.of("system-property", "io.undertow.ajp.REQUIRE_AJP_SECRET"),
+                Values.of("value", ModelNode.FALSE)).assertSuccess();
+        new Administration(client).restart();
     }
 
     @AfterClass
